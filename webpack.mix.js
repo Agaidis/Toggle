e77f3123d3195d9082e3ba49a0471fb527789c6a
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-10-04 21:34:45 -0400

    committing toggle

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,41 @@
 const mix = require('laravel-mix');
-
+mix.autoload({
+    jquery: ['$', 'window.jQuery',"jQuery","window.$","jquery","window.jquery"]
+});
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js([
+    'resources/assets/js/bootstrap.js',
+    'vendor/select2/select2/dist/js/select2.min.js',
+    'resources/assets/js/jquery-dp-ui.min.js',
+    'resources/assets/js/leasePage.js',
+    'resources/assets/js/wellbore.js',
+    'resources/assets/js/permits.js',
+    'resources/assets/js/admin.js',
+    'resources/assets/js/phoneNumberPush.js',
+    'resources/assets/js/owner.js',
+    'resources/assets/js/datatables.min.js',
+    'resources/assets/js/permitStorage.js'
+
+], 'public/js/app.js').version()
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .styles([
+        'public/css/app.css',
+        'resources/assets/css/mmp.css',
+        'resources/assets/css/datatables.min.css',
+        'resources/assets/css/jquery-dp-ui.min.css',
+        'resources/assets/css/jquery-dp-ui.structure.min.css',
+        'resources/assets/css/jquery-dp-ui.theme.min.css',
+        'vendor/select2/select2/dist/css/select2.min.css'
+
+
+    ], 'public/css/app.css').version();
