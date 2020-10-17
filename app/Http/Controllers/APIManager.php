15228c946f5799c0ef063dd4566c706f90f8e99d
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-10-17 13:43:23 -0400

    commenting out wells for now

--- app/Http/Controllers/APIManager.php
+++ app/Http/Controllers/APIManager.php
@@ -107,7 +107,6 @@ class APIManager
             $url = str_replace(['\'', ' '], ['', ''], $url);
 
             $curl = curl_init();
-            Log::info($url);
 
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $url,
@@ -125,7 +124,6 @@ class APIManager
                     "Authorization: Bearer " . $token
                 ),
             ));
-
 
 
             $response = curl_exec($curl);
@@ -166,6 +164,72 @@ class APIManager
             $errorMsg->save();
         }
     }
+
+    public function getLegalLeases ($county, $token, $linkUrl) {
+
+        if ($linkUrl == '') {
+
+            $url = "https://di-api.drillinginfo.com/v2/direct-access/legal-leases?countyparish=".$county."&pagesize=2000";
+        } else {
+            $url = "https://di-api.drillinginfo.com/v2/direct-access/legal-leases" . $linkUrl;
+        }
+
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 50,
+                CURLOPT_HEADER => 1,
+                CURLOPT_FOLLOWLOCATION => false,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "x-api-key: e89c4d8b6edf1a7b5c9739e6ae5e4235",
+                    "Accept: */*",
+                    "Authorization: Bearer " . $token
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            $response = rtrim($response);
+            $response = explode('Connection: keep-alive', $response);
+
+            $headerInfo = explode("\n",$response[0]);
+            $jsonData = ltrim($response[1]);
+
+            if (isset($headerInfo[5])) {
+                $nextUrl = str_replace(['Links:','</legal-leases', '>; rel="next"'], ['','',''], $headerInfo[5]);
+            } else {
+                $nextUrl = '';
+            }
+
+            if (strpos($nextUrl, 'openresty')) {
+                $nextUrl = '';
+            }
+
+            $responseWithLink = [0 => $jsonData, 1 => $nextUrl];
+
+            if ($err) {
+                return "cURL Error #:" . $err;
+            } else {
+                return $responseWithLink;
+            }
+        } catch ( Exception $e ) {
+            $errorMsg = new ErrorLog();
+            $errorMsg->payload = $e->getMessage() . ' Line #: ' . $e->getLine() . ' File: ' . $e->getFile();
+
+            $errorMsg->save();
+        }
+    }
+
 
     public function getLandtracLeases ($county, $token, $linkUrl) {
 
@@ -222,74 +286,6 @@ class APIManager
             }
 
             $responseWithLink = [0 => $jsonData, 1 => $nextUrl];
-
-            if ($err) {
-                return "cURL Error #:" . $err;
-            } else {
-                return $responseWithLink;
-            }
-        } catch ( Exception $e ) {
-            $errorMsg = new ErrorLog();
-            $errorMsg->payload = $e->getMessage() . ' Line #: ' . $e->getLine() . ' File: ' . $e->getFile();
-
-            $errorMsg->save();
-        }
-    }
-
-    public function getLegalLeases ($county, $token, $linkUrl) {
-
-        if ($linkUrl == '') {
-
-            $url = "https://di-api.drillinginfo.com/v2/direct-access/legal-leases?countyparish=".$county."&pagesize=2000";
-        } else {
-            $url = "https://di-api.drillinginfo.com/v2/direct-access/legal-leases" . $linkUrl;
-        }
-
-        try {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 50,
-                CURLOPT_HEADER => 1,
-                CURLOPT_FOLLOWLOCATION => false,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_HTTPHEADER => array(
-                    "x-api-key: e89c4d8b6edf1a7b5c9739e6ae5e4235",
-                    "Accept: */*",
-                    "Authorization: Bearer " . $token
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            $response = rtrim($response);
-            $response = explode('Connection: keep-alive', $response);
-
-            $headerInfo = explode("\n",$response[0]);
-            $jsonData = ltrim($response[1]);
-
-           // Log::info($headerInfo[5]);
-            if (isset($headerInfo[5])) {
-                $nextUrl = str_replace(['Links:','</legal-leases', '>; rel="next"'], ['','',''], $headerInfo[5]);
-            } else {
-                $nextUrl = '';
-            }
-
-            if (strpos($nextUrl, 'openresty')) {
-                $nextUrl = '';
-            }
-
-            $responseWithLink = [0 => $jsonData, 1 => $nextUrl];
-
-            Log::info('Response Link: ' . $responseWithLink[1]);
 
             if ($err) {
                 return "cURL Error #:" . $err;
