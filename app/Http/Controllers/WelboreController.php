848d7df3c227f4b749ef0244f07302dc3e53c6c2
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2021-01-25 09:33:06 -0500

    yep

--- app/Http/Controllers/WelboreController.php
+++ app/Http/Controllers/WelboreController.php
@@ -48,14 +48,14 @@ class WelboreController extends Controller
             }
 
                 //In here override the current user if the userId is not 0
-                $highPriorityProspects = DB::select('select id, follow_up_date, lease_name, assignee, wellbore_type, owner, owner_address, owner_city, owner_zip, owner_decimal_interest, owner_interest_type, Grantor, GrantorAddress  from mineral_owners WHERE assignee = ' . $user . ' AND wellbore_type != "0"  ORDER BY FIELD(wellbore_type, "4", "3", "2", "1" ), wellbore_type DESC');
+                $highPriorityProspects = DB::select('select id, follow_up_date, lease_name, assignee, wellbore_type, owner, owner_address, owner_city, owner_zip, owner_decimal_interest, owner_interest_type, Grantor, GrantorAddress  from mineral_owners WHERE assignee = ' . $user . ' AND wellbore_type != "0" GROUP BY owner ORDER BY FIELD(wellbore_type, "4", "3", "2", "1" ),  wellbore_type DESC');
 
                 $owners = DB::table('mineral_owners')
                     ->where('follow_up_date', '!=', NULL)
                     ->where('mineral_owners.assignee', $user)
                     ->join('permits', function ($join) {
                         $join->on('mineral_owners.lease_name', '=', 'permits.lease_name')
-                            ->orOn('mineral_owners.lease_name', '=', 'permits.selected_lease_name');
+                            ->orOn('mineral_owners.lease_name', '=', 'permits.selected_lease_name')->groupBy('mineral_owners.owner');
                     })
                //     ->join('permits', 'mineral_owners.lease_name', '=', 'permits.lease_name')
                     ->select('mineral_owners.id', 'mineral_owners.follow_up_date', 'mineral_owners.lease_name', 'mineral_owners.assignee', 'mineral_owners.wellbore_type', 'owner', 'owner_address', 'owner_city', 'owner_zip', 'owner_decimal_interest', 'owner_interest_type', 'Grantor', 'GrantorAddress')
