a5ab9fb5d14ad908cd9a537b2b842037e6722982
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2021-11-01 14:22:54 -0400

    ability to finally update these guys

--- app/Http/Controllers/UploadTMAController.php
+++ app/Http/Controllers/UploadTMAController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\ErrorLog;
+use App\MineralOwner;
 use App\MineralOwners;
 use Exception;
 use Maatwebsite\Excel\Facades\Excel;
@@ -34,7 +35,94 @@ class UploadTMAController extends Controller
                 $file = 'notworking';
             }
 
-            Excel::import(new MineralOwners(), storage_path('app/public/tma.csv'));
+            // Read a CSV file
+            $handle = fopen("../storage/app/public/tma.csv", "r");
+            $lineNumber = 1;
+
+            while (($raw_string = fgets($handle)) !== false) {
+                // Parse the raw csv string: "1, a, b, c"
+                $row = str_getcsv($raw_string);
+
+                try {
+                    $owner = $row[0];
+                    $ownerAddress = $row[1];
+                    $ownerCity = $row[2];
+                    $ownerState = $row[3];
+                    $ownerZip = $row[4];
+                    $ownerDecimalInterest = $row[5];
+                    $ownerInterestType = $row[6];
+                    $appraisalYear = $row[7];
+                    $operatorCompanyName = $row[8];
+                    $reportedOperatorName = $row[9];
+                    $operatorTicket = $row[10];
+                    $leaseName = $row[11];
+                    $rrcLeaseNumber = $row[12];
+                    $county = $row[13];
+                    $stateProvince = $row[14];
+                    $diBasin = $row[15];
+                    $diPlay = $row[16];
+                    $diSubplay = $row[17];
+                    $taxValue = $row[18];
+                    $api10 = $row[19];
+                    $leaseDescription = $row[20];
+                    $rrcDistrict = $row[21];
+                    $firstProdDate = $row[22];
+                    $lastProdDate = $row[23];
+                    $cumProdOil = $row[24];
+                    $cumProdGas = $row[25];
+                    $activeWellCount = $row[47];
+
+                    $isOwnerExist = MineralOwner::where('owner', $owner)->where('lease_name', $leaseName)->get();
+
+                    if ($isOwnerExist->isEmpty()) {
+
+                        $newOwner = new MineralOwner();
+                        $newOwner->owner = $owner;
+                        $newOwner->lease_name = $leaseName;
+                        $newOwner->operator_company_name = $operatorCompanyName;
+                        $newOwner->owner_address = $ownerAddress;
+                        $newOwner->owner_city = $ownerCity;
+                        $newOwner->owner_state = $ownerState;
+                        $newOwner->owner_zip = $ownerZip;
+                        $newOwner->owner_decimal_interest = $ownerDecimalInterest;
+                        $newOwner->owner_interest_type = $ownerInterestType;
+                        $newOwner->appraisal_year = $appraisalYear;
+                        $newOwner->rrc_lease_number = $rrcLeaseNumber;
+                        $newOwner->county = $county;
+                        $newOwner->state = $stateProvince;
+                        $newOwner->tax_value = $taxValue;
+                        $newOwner->lease_description = $leaseDescription;
+                        $newOwner->first_prod_date = $firstProdDate;
+                        $newOwner->last_prod_date = $lastProdDate;
+                        $newOwner->cum_prod_oil = $cumProdOil;
+                        $newOwner->cum_prod_gas = $cumProdGas;
+                        $newOwner->active_well_count = $activeWellCount;
+
+                        $newOwner->save();
+
+                    } else {
+                        MineralOwner::where('id', $isOwnerExist[0]->id)->update([
+                            'tax_value' => $taxValue,
+                            'last_prod_date' => $lastProdDate,
+                            'cum_prod_oil' => $cumProdOil,
+                            'cum_prod_gas' => $cumProdGas,
+                            'lease_description' => $leaseDescription
+                        ]);
+                    }
+
+                } catch ( Exception $e ) {
+                    $errorMsg = new ErrorLog();
+                    $errorMsg->payload = $e->getMessage() . ' Line #: ' . $e->getLine();
+                    $errorMsg->save();
+                }
+
+                // Increase the current line
+                $lineNumber++;
+            }
+
+            fclose($handle);
+
+       //     Excel::import(new MineralOwners(), storage_path('app/public/tma.csv'));
 
             return redirect('/upload-tma')->with('message', 'Owners are Updated!');
 
--- app/MineralOwners.php
+++ app/MineralOwners.php
@@ -19,56 +19,7 @@ class MineralOwners implements ToModel, WithChunkReading
      */
     public function model(array $row)
     {
-        try {
-            $owner = $row[0];
-            $ownerAddress = '';
-            $leaseName = $row[11];
-            $taxValue = $row[18];
-            $lastProdDate = $row[23];
-            $cumProdOil = $row[24];
-            $cumProdGas = $row[25];
 
-            $isOwnerExist = MineralOwner::where('owner', $owner)->where('lease_name', $leaseName)->get();
-
-            if ($isOwnerExist->isEmpty()) {
-//                $newOwner = new MineralOwner();
-//                $newOwner->owner = $owner;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//                $newOwner-> ;
-//
-//                $newOwner->save();
-
-            } else {
-                MineralOwner::where('id', $isOwnerExist[0]->id)->update([
-                    'tax_value' => $taxValue,
-                    'last_prod_date' => $lastProdDate,
-                    'cum_prod_oil' => $cumProdOil,
-                    'cum_prod_gas' => $cumProdGas,
-                ]);
-            }
-
-        } catch ( Exception $e ) {
-            $errorMsg = new ErrorLog();
-            $errorMsg->payload = $e->getMessage() . ' Line #: ' . $e->getLine();
-            $errorMsg->save();
-        }
     }
 
     public function startRow(): int
