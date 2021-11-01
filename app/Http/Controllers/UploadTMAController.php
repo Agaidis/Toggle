c0ba5453a1fa8bb8101b418a2a3ca5be60002395
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2021-11-01 15:41:09 -0400

    ability to finally update these guys

--- app/Http/Controllers/UploadTMAController.php
+++ app/Http/Controllers/UploadTMAController.php
@@ -37,11 +37,16 @@ class UploadTMAController extends Controller
 
             // Read a CSV file
             $handle = fopen("../storage/app/public/tma.csv", "r");
-            $lineNumber = 1;
+            $lineNumber = 0;
 
             while (($raw_string = fgets($handle)) !== false) {
+
                 // Parse the raw csv string: "1, a, b, c"
                 $row = str_getcsv($raw_string);
+
+                $errorMsg = new ErrorLog();
+                $errorMsg->payload = serialize($row);
+                $errorMsg->save();
 
                 try {
                     $owner = $row[0];
@@ -64,13 +69,13 @@ class UploadTMAController extends Controller
                     $diSubplay = $row[17];
                     $taxValue = $row[18];
                     $api10 = $row[19];
-                    $leaseDescription = $row[20];
+                    $leaseDescription = $row[21];
                     $rrcDistrict = $row[21];
-                    $firstProdDate = $row[22];
-                    $lastProdDate = $row[23];
-                    $cumProdOil = $row[24];
-                    $cumProdGas = $row[25];
-                    $activeWellCount = $row[47];
+                    $firstProdDate = $row[23];
+                    $lastProdDate = $row[24];
+                    $cumProdOil = $row[25];
+                    $cumProdGas = $row[26];
+                    $activeWellCount = $row[20];
 
                     $isOwnerExist = MineralOwner::where('owner', $owner)->where('lease_name', $leaseName)->get();
 
@@ -103,6 +108,7 @@ class UploadTMAController extends Controller
                     } else {
                         MineralOwner::where('id', $isOwnerExist[0]->id)->update([
                             'tax_value' => $taxValue,
+                            'first_prod_date' => $firstProdDate,
                             'last_prod_date' => $lastProdDate,
                             'cum_prod_oil' => $cumProdOil,
                             'cum_prod_gas' => $cumProdGas,
