f6e74fc95d83b03eccd285e715f06788b35fc079
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2022-06-02 11:16:09 -0400

    fixing lease creator and adding la salle

--- app/Http/Controllers/LeaseCreatorController.php
+++ app/Http/Controllers/LeaseCreatorController.php
@@ -22,7 +22,7 @@ class LeaseCreatorController extends Controller
     public function createLease(Request $request) {
         try {
 
-            $eagleInterestCountiesArray = array('ATASCOSA','BEE','DEWITT','GONZALES','KARNES','LIVE OAK','LAVACA','WILSON');
+            $eagleInterestCountiesArray = array('ATASCOSA','BEE','DEWITT','GONZALES','KARNES','LIVE OAK','LAVACA','WILSON', 'LA SALLE');
             $wtxInterestCountiesArray = array('ANDREWS', 'DAWSON', 'GAINES', 'BORDEN', 'CRANE', 'ECTOR', 'STERLING', 'MITCHELL', 'JEFF DAVIS');
 
             $etxInterestCountiesArray = array('CASS', 'GREGG', 'HARRISON', 'MARION', 'MORRIS', 'NACOGDOCHES', 'PANOLA', 'SAN AUGUSTINE', 'RUSK', 'SHELBY', 'UPSHUR');
